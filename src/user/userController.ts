cb860e03acc1301148f02066679eb47ac88781bb
Zubair <h.m.zubairrafiq@gmail.com>
2024-10-03 12:02:49 +0500

    Regsitration with hashing and creating token for web

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
-
+import UserModel from "./UserModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
 export const createUser = async (
     req: Request,
     res: Response,
@@ -12,6 +14,28 @@ export const createUser = async (
         const error = createHttpError(400, "All fields are required");
         return next(error);
     }
-    console.log(name, email, password);
-    res.json({ message: "Create User" });
+    //check if user already exists
+    const user = await UserModel.findOne({ email });
+    if (user) {
+        const error = createHttpError(400, "user already exists");
+        return next(error);
+    }
+    //Hashing Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //Saving the New User
+    const newUser = new UserModel({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    const token = sign({ sub: newUser._id }, process.env.JWT_SECRET!, {
+        expiresIn: "7d",
+    });
+    await newUser.save();
+    res.status(201).json({
+        message: "User created successfully",
+        id: newUser._id,
+        token,
+    });
 };
